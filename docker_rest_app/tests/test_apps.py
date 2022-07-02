5d7111fdbd72f4651a62f151816ac4a4033bc895
Abdul Mateen <abdul.mateen@>
2022-07-02 19:50:35 +0200

    Fixed testcases

--- docker_rest_app/tests/test_apps.py
+++ docker_rest_app/tests/test_apps.py
@@ -10,8 +10,8 @@ class TestApp(TestCase):
     dockerfile: str = b"From alpine:latest \n RUN apk add --no-cache curl"
     image_name: str = 'test_image'
     tag: str = '0.0.1'
-    docker_url: str = 'http://localhost:8282/api/v2/docker/build'
-    task_url: str = 'http://localhost:8282/api/v2/docker/build/task'
+    docker_url: str = 'http://localhost:8080/api/v2/docker/build'
+    task_url: str = 'http://localhost:8080/api/v2/docker/task'
 
     @staticmethod
     def _start_container() -> None:
@@ -57,21 +57,22 @@ class TestApp(TestCase):
 
     def test_task_completion(self) -> None:
         task_id = self._send_docker_build_request(dockerfile=self.dockerfile)
-        time.sleep(30)
-        response = requests.get(f'{self.task_url}/{str(task_id)}')
+        time.sleep(15)
+        response = requests.get(f'{self.task_url}/{str(*task_id)}')
+        print(response.json())
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['state'], 'SUCCESS')
 
     def test_task_failure(self) -> None:
         task_id = self._send_docker_build_request(dockerfile=f'@@@@++{self.dockerfile}``++')
         time.sleep(25)
-        response = requests.get(f'{self.task_url}/{str(task_id)}')
+        response = requests.get(f'{self.task_url}/{str(*task_id)}')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['state'], 'FAILURE')
+        self.assertEqual(response.json()['state'][0], 'FAILURE')
 
     def test_task_started_or_pending(self) -> None:
         task_id = self._send_docker_build_request(dockerfile=self.dockerfile)
-        response = requests.get(f'{self.task_url}/{str(task_id)}')
+        response = requests.get(f'{self.task_url}/{str(*task_id)}')
         time.sleep(1)
         self.assertEqual(response.status_code, 200)
         self.assertIn(response.json()['state'], ['STARTED', 'PENDING'])
